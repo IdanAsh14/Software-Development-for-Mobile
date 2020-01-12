f00f578486607db1710ce192d2a2b80c13ff0dda
Idan Ashkenazi <idan2414@gmail.com>
2020-01-12 19:30:54 +0200

    update server side to support FCM

--- Exercise05/server/index.js
+++ Exercise05/server/index.js
@@ -2,54 +2,116 @@ const express = require('express');
 const request = require('request');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
+const serviceAccountKey = require('/Users/idan/Desktop/alpha-vantage-stock-firebase-adminsdk-vrbad-bfb18115b6.json')
 
 const PORT = 8080;
-let apiKey = '3IGR30B8QW9DPS2R';
+let alphavantgeApiKey = '3IGR30B8QW9DPS2R';
+let FCM_SERVER_KEY = 'AIzaSyCDB94gLh72qKhR81LXJTPyLTJ-NIr3jHc';
 const DEFAULT_STOCK = 'MSFT'; // microsoft stock
+var tokens = {};
+var id_counter = 1;
+var intervalId = -1;
 
 let app = express();
+let firebaseApp = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccountKey),
+    databaseURL: '"https://alpha-vantage-stock.firebaseio.com"'
+});
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => { 
     res.send("Welcome to Idan's stock fetcher server");
 });
 
-// default stock 
-app.get('/stock', (req, res) => { 
-    let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${DEFAULT_STOCK}&apikey=${apiKey}`;
-    request(url, function (err, response, body) {
-        if(err){
-            return res.status(500).json({err: err.message});
-        } else {
-            let stock = JSON.parse(body);
-            let message = `${stock["Global Quote"]["01. symbol"]} stock price is ${stock["Global Quote"]["05. price"]}.`;
-            console.log(message);
-            return res.status(200).json(stock);
+//get token from client
+app.post('/:user/token', (req, res, next) => {
+    let token = req.body.token;
+    if (!token) return res.status(400).json({err: "missing token"});
+    
+    let id = isTokenExist(token);
+    let obj = {};
+    //new users
+    if(id === -1){
+        tokens[id_counter] = {"token": token, "stock": ''};
+        obj = {"id": id_counter};
+        console.log(`Received new token request from ${id_counter} for token=${token}`);
+        id_counter++;
+    }
+    //exist user, new token
+    else {
+        tokens[id]["token"] = token;
+        obj = {"id": id};
+        console.log(`Received token request from ${id} for token=${token}`);
+    }
+   
+    res.status(200).json(obj);
+});
+
+function isTokenExist(token){
+    var id = -1;
+    Object.keys(tokens).forEach(key => {
+        if(tokens[key]["token"] === token){
+            id = key;
         }
     });
-});
+    return id;
+}
 
 // user request stock 
 app.post('/stock', (req, res) => {
     console.dir(req.body);
     let newStock = req.body.stock || DEFAULT_STOCK;
+    let id = req.body.id;
+    tokens[id]["stock"] =  newStock;
     console.log("Got POST request to /stock, using stock=" + newStock);
+    getStock(id);
+    if(intervalId !== -1){
+        clearInterval(intervalId);
+        intervalId = -1;
+    }
+    intervalId = setInterval(getStock, 15000, [id]);
+    // getStock(id);
+    return res.status(200);
+});
 
-    let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${newStock}&apikey=${apiKey}`;
+function getStock(id){
+    let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${tokens[id]["stock"]}&apikey=${alphavantgeApiKey}`;
     request(url, function (err, response, body) {
         if(err){
-            return res.status(500).json({err: err.message});
+            return err;
         } else {
             let stock = JSON.parse(body);
-            if(stock.hasOwnProperty('Error Message')){
-                return res.status(500).json({err: stock['Error Message']});
-            }
-            let message = `${stock["Global Quote"]["01. symbol"]} stock price is ${stock["Global Quote"]["05. price"]}.`;
+            let stockName = stock["Global Quote"]["01. symbol"];
+            let stockPrice = stock["Global Quote"]["05. price"];
+            let message = `${stockName} stock price is ${stockPrice}.`;
             console.log(message);
-            return res.status(200).json(stock);
+            sendMessageToFCM(id, stockName, stockPrice);
+            // return;
         }
     });
-});
+    // return;
+}
+
+function sendMessageToFCM(id, stockName, price){
+    var message = {
+        data: {
+        name: stockName,
+        price: price
+        },
+        token: tokens[id]["token"]
+    };
+    
+    admin.messaging().send(message)
+    .then((response) => {
+        // Response is a message ID string.
+        console.log('Successfully sent message:', response);
+    })
+    .catch((error) => {
+        console.log('Error sending message:', error);
+    });
+    // return;
+}
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
